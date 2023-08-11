56ef581b4740f4be0ad677ca5ab0f5d1b206465e
abir Jmail <jmailabir@gmail.com>
2023-08-11 17:31:51 +0200

    Comment entity change content type text

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -13,7 +13,7 @@ class Comment
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
 
     #[ORM\Column]
