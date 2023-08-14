18c00149c155dbcac99dd6fca897893c997ce58a
abir Jmail <jmailabir@gmail.com>
2023-08-15 00:17:37 +0200

    hashPassword

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,10 +6,11 @@ use App\Entity\User;
 use App\Form\User1Type;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/profile')]
 class ProfileController extends AbstractController
@@ -52,13 +53,19 @@ class ProfileController extends AbstractController
     }
 
     #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager,UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = $this ->getUser();
         $form = $this->createForm(User1Type::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashPassword = $passwordHasher->hashPassword(
+               $user,
+               $plainPassword 
+            );
+            $user->setPassword($hashPassword);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_profile_show', [], Response::HTTP_SEE_OTHER);
--- src/Form/User1Type.php
+++ src/Form/User1Type.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,9 @@ class User1Type extends AbstractType
         $builder
             ->add('email')
             // ->add('roles')
-            ->add('password')
-            ->add('isVerified')
+            ->add('plainPassword',TextType::class,
+            ['mapped' => false])
+            // ->add('isVerified')
         ;
     }
 
