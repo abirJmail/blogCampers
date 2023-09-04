7b91157d36687ea6725641e3ca1de3787e9bbe12
abir Jmail <jmailabir@gmail.com>
2023-09-04 14:00:01 +0200

    login css and

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,9 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
@@ -20,10 +25,43 @@ class UserType extends AbstractType
                     'class' => 'form-control mb-3 w-50'
                 ],  ])
             // ->add('roles')
-            ->add('plainPassword', TextType::class, ['mapped' => false,
-            'attr' => [
-                'class' => 'form-control mb-3 w-50'
-            ],])
+            // ->add(
+            //     'roles', 'choice', [
+            //         'choices' => ['ROLE_ADMIN' => 'ROLE_ADMIN', 'ROLE_USER' => 'ROLE_USER', 'ROLE_CUSTOMER' => 'ROLE_CUSTOMER'],
+            //         'expanded' => true,
+            //         'multiple' => true,
+            //     ]
+            // )
+            // ->add('plainPassword', TextType::class, ['mapped' => false,
+            // 'attr' => [
+            //     'class' => 'form-control mb-3 w-75'
+            // ],])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'label' => 'Mot de passe',
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                    new Regex( 
+                        pattern: '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
+                        message: 'Votre mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial',
+                    )
+                ],
+                'attr' => [
+                    'class' => 'mb-3 form-control'
+                ]
+            ])
+
             ->add('isVerified')
         ;
     }
